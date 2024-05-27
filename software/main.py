a0022bc3bf25878e223b3fa8b5c8c39fce9ec7f0
Jacob Cunningham <jcun4128@gmail.com>
2024-05-27 15:10:02 -0500

    test wiring

--- software/main.py
+++ software/main.py
@@ -1,5 +1,12 @@
+import time
+
 from imu.imu import Imu
 
 # human POV (looking at device)
 imu_left = Imu(69)
 imu_right = Imu(68)
+
+while (True):
+  print('left ' + imu_left.accel[2])
+  print('right ' + imu_left.accel[2])
+  time.sleep(0.1)
