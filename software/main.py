e5e5af944296c1d2e4e105ddf96ae5e1db290b36
Jacob Cunningham <jcun4128@gmail.com>
2024-05-27 15:26:05 -0500

    fix

--- software/main.py
+++ software/main.py
@@ -6,7 +6,10 @@ from imu.imu import Imu
 imu_left = Imu(0)
 imu_right = Imu(1)
 
+imu_left.start()
+imu_right.start()
+
 while (True):
   print('left ' + str(imu_left.accel[2]))
-  print('right ' + str(imu_left.accel[2]))
+  print('right ' + str(imu_right.accel[2]))
   time.sleep(0.1)
