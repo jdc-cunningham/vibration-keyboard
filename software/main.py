d81e10871cb12cbf548e8a67c46d9531d0b7f3fa
Jacob Cunningham <jcun4128@gmail.com>
2024-06-03 12:24:15 -0500

    turn led on then off

--- software/main.py
+++ software/main.py
@@ -1,13 +1,19 @@
 import time
 
 from imu.imu import Imu
+from led.led import Led
 
 # human POV (looking at device)
 imu_left = Imu(0)
 imu_right = Imu(1)
+status_led = Led()
 
 imu_left.start()
 imu_right.start()
+
+status_led.turn_on()
+time.sleep(1)
+status_led.turn_off()
 
 while (True):
   print('left ' + str(imu_left.accel[2]))
